ebf38ad5e3f0647f59949a10a85ffc908dac9cb8
mdmjg <mdj308@nyu.edu>
2020-10-10 19:54:39 -0500

    tried fixing linting error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,9 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = "test@medellin.com"
         password = "test123"
-        user = get_user_model().objects.create_user(email, password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )  # noqa
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -25,7 +27,9 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser("test@medellin.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@medellin.com", "test123"
+        )  # noqa
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
