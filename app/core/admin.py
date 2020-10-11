3c061ea7ecf87974b198768237f151149f05e7df
mdmjg <mdj308@nyu.edu>
2020-10-11 09:39:28 -0500

    admin to support user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,24 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from core import models
 
-# # Register your models here.
+
+class UserAdmin(BaseUserAdmin):  # all of this is standard django admin
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("name",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",)}),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
+    # wide means it will make the input field fullscreen
+    # fieldsets are fields to be used when editing users
+    # add_fieldsets are fields used when creating users
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -8,19 +8,18 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """Creates and saves a new User"""
         if not email:
             raise ValueError("Users must have an email address")
-        user = self.model(
-            email=self.normalize_email(email), **extra_fields
-        )  # creates a new user model (a row?)
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)  # works for when we have several databases
+        user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves new superuser"""
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -30,15 +29,32 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instad of username"""
+    """Custom user model that supports using email instead of username"""
 
-    email = models.EmailField(
-        max_length=255, unique=True
-    )  # you can only create one user with one email
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"  # customizing
+    USERNAME_FIELD = "email"
+
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new User"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
