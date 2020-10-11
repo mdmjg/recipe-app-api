fafd4fb8376b6cc60244729ec70c08e44d32eb26
mdmjg <mdj308@nyu.edu>
2020-10-11 09:55:12 -0500

    edited formatted

--- app/core/models.py
+++ app/core/models.py
@@ -48,13 +48,3 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,9 +7,7 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = "test@medellin.com"
         password = "test123"
-        user = get_user_model().objects.create_user(
-            email=email, password=password
-        )  # noqa
+        user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
